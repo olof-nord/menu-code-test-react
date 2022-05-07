e60a8c019f4e497e4b6ce765f07efb45961bcb9c
Olof Nord <olof.nord@tutanota.com>
2022-05-07 13:36:51 +0200

    Further improvement on the jest test coverage gathering

--- backend.jest.config.js
+++ backend.jest.config.js
@@ -3,10 +3,12 @@ module.exports = {
         '<rootDir>/src/server/utils/setupBackendTests.js'
     ],
     collectCoverage: true,
+    collectCoverageFrom: ['src/server/**/*.js'],
     coveragePathIgnorePatterns: [
         '<rootDir>/node_modules/',
         '<rootDir>/dist/',
         '<rootDir>/src/components/',
         '<rootDir>/src/utils/',
+        '<rootDir>/src/server/static/',
     ]
 };
--- frontend.jest.config.js
+++ frontend.jest.config.js
@@ -8,6 +8,7 @@ module.exports = {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/src/utils/fileMock.js'
     },
     collectCoverage: true,
+    collectCoverageFrom: ['src/**/*.js'],
     coveragePathIgnorePatterns: [
         '<rootDir>/node_modules/',
         '<rootDir>/dist/',
